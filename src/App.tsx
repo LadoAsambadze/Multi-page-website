12db765b0b747505c399413a01c3b0a5e42f8ef7
Lado Asambadze <lado.asambadze1@gmail.com>
2023-05-25 13:30:46 +0400

    fix: move back deleted photos...

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Header from "./Components/Header";
 import { Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
 import Footer from "./Components/Footer";
+import Stories from "./Pages/Stories";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/stories" element={<Stories />} />
       </Routes>
       <Footer />
     </>
